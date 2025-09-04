dbe572e9ba9a68ae31e05fb0c84149c686adcd47
Sidharth Arora <sidhartharora1122@gmail.com>
2025-09-04 17:26:52 +0530

    Setup.py file ready

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup  
+from typing import List
+
+def get_requirements() -> List[str]:
+    requirements_list:List[str] = []
+    try :
+        with open('requirements.txt') as file:
+            lines = file.readlines()
+
+            for line in lines:
+                requirements = line.strip()
+                if requirements and requirements != '-e .':
+                    requirements_list.append(requirements)
+
+    except Exception as e:
+        print('requirements.txt file is not found')
+
+    return requirements_list
+
+setup(
+    name = 'networksecurity',
+    version = '0.1',
+    author = 'Sidharth Arora',
+    author_email = 'sidhartharora1122@example.com',
+    packages = find_packages(),
+    install_requires = get_requirements()
+)
